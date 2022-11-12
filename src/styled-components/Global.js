7c45a1470e14a4220c7f8b406a298f9e3cd64c51
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-12 12:40:10 +0100

    Created my typography file forrr font-sizes

--- src/styled-components/Global.js
+++ src/styled-components/Global.js
@@ -1,19 +1,26 @@
 import { createGlobalStyle } from "styled-components";
-import { primaryFont } from "./typography";
 import { normalize } from "polished";
 
 export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital@1&display=swap');
+
    ${normalize()}
   
     *, *:before, *:after {
-        box-sizing: inherit;
-      
+        box-sizing: border-box;
+        margin : 0;
+        padding : 0;  
     }
 
 
 img {
     max-width: 100%;
     height: auto;
+    object-fit : cover;
+}
+
+a {
+    display : block;
+    text-decoration : none;
 }
 `;
