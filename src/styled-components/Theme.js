c1569d9035b453de900e3c63cd10961942847040
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-12 12:45:41 +0100

    Returning back to structure my Theme.js file

---
+++ src/styled-components/Theme.js
@@ -0,0 +1,25 @@
+import { blue, neutral, red, yellow } from "./Colors";
+
+export const defaultTheme = {
+  primaryColor: blue[300],
+  primaryHoverColor: blue[200],
+  primaryActiveColor: blue[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  status: {
+    status1: red[100],
+  },
+};
+
+export const darkTheme = {
+  primaryColor: yellow[300],
+  primaryHoverColor: yellow[200],
+  primaryActiveColor: yellow[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  status: {
+    status1: red[100],
+  },
+};
