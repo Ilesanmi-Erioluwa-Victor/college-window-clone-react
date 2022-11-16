77adb9711f58c9113833f03b10dd76e00b17a67d
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-16 16:59:18 +0100

    Exported all my Images

--- src/Utils/Images.js
+++ src/Utils/Images.js
@@ -1,10 +1,53 @@
-
+import Arrowdown from "../Assets/Arrowdown.svg"
+import Calender from "../Assets/Calendar.svg"
+import Closecamera from "../Assets/Closecamera.svg"
+import Createclass from "../Assets/Createclass.svg"
+import Earning from "../Assets/Earning.svg";
+import Element from "../Assets/Element.svg";
+import Endcall from "../Assets/Endcall.svg";
+import Logout from "../Assets/Logout.svg";
+import Message from "../Assets/Message.svg";
+import Messageavatar from "../Assets/Messageavatar.png";
+import Microphone from "../Assets/Microphone.svg";
+import Notificationicon from "../Assets/Notificationicon.svg";
+import Overview from "../Assets/Overview.svg";
+import Send from "../Assets/Send.svg";
+import Setting from "../Assets/Setting.svg";
+import Tagcross from "../Assets/Tagcross.svg";
+import Useravatar from "../Assets/Useravatar.png";
+import Video from "../Assets/Video.svg";
+import Videocreate from "../Assets/Videocreate.svg";
+import Walletmoney from "../Assets/Walletmoney.svg";
 
 const Image = ({src, className, alt}) => {
-    return <img src={src} className={className} alt={alt} loading="lazy"/>
+    return (
+      <figure>
+        <img src={src} className={className} alt={alt} loading="lazy" />
+      </figure>
+    );
 };
 
 
 export {
     Image,
+    Arrowdown,
+    Message,
+    Notificationicon,
+    Element,
+    Calender,
+    Walletmoney,
+    Videocreate,
+    Video,
+    Tagcross,
+    Useravatar,
+    Setting,
+    Send,
+    Overview,
+    Microphone,
+    Messageavatar,
+    Logout,
+    Endcall,
+    Earning,
+    Createclass,
+    Closecamera
 }
