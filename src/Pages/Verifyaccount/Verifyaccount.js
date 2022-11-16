1eaba18ea6ed577c1a00ac4f14a44e7583515c0c
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-16 18:05:14 +0100

    Done with pages

---
+++ src/Pages/Verifyaccount/Verifyaccount.js
@@ -0,0 +1,5 @@
+const Verifyaccount = () => {
+  return <h1>Hello from Verifyaccount</h1>;
+};
+
+export default Verifyaccount;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,2 +1,8 @@
 export { default as Error } from "./404/Error";
 export { default as Classroom } from "./Classroom/Classroom";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Login } from "./Login/Login";
+export { default as Payment } from "./Payment/Payment";
+export { default as Signup } from "./Signup/Signup";
+export { default as StudentDashboard } from "./StudentDashboard/StudentDashboard";
+export { default as Verifyaccount } from "./Verifyaccount/Verifyaccount";
