ca92d6760d11ad2489c442b551ec61d51a620b8b
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-18 10:39:48 +0100

    Consumed my sign up user Api

--- src/Auths/Users/users.js
+++ src/Auths/Users/users.js
@@ -1,5 +1,21 @@
 import { CollegeBaseUri } from "../../Utils/BaseUri";
 
-export const registerUser = async ({name,password, email})=> {
+export const registerUser = async ({ name, password, email }) => {
+  try {
+    const Response = await fetch(`${CollegeBaseUri}/student_signup/`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
 
-}
+      body: JSON.stringify({ email, password, name }),
+    });
+
+    const feedBack = await Response.json();
+
+    console.log(feedBack)
+  } catch (error) {
+    console.log(error)
+  }
+};
