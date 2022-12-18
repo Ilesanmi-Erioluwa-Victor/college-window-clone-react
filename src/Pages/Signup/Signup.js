8e3b2dc40eabdc3128087e8c5c1b6ab0c27b61cc
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-18 10:13:33 +0100

    Working on Signup Page

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { NavLink, useNavigate } from "react-router-dom";
+import { DropdownDate } from "react-dropdown-date";
+
 
 const Signup = () => {
-  return <h1>Hello from Signup</h1>;
+  const naviagte = useNavigate();
+  const [uploading, setUploading] = useState(false);
+  const [img, setImg] = useState("");
+  
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default Signup;
