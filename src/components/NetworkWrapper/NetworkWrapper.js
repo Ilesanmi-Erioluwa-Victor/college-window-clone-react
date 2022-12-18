94b6fa638e9b4ffd7fdfe2e7ea8bd4582940f329
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-18 08:32:06 +0100

    Created a network Wrapper for checking user network availability

---
+++ src/components/NetworkWrapper/NetworkWrapper.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
+
+export default function NetWorkWrapper({ children }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window &&
+        window.addEventListener(
+          "your offline, please check your internet connection",
+          function (e) {
+            toast.error("You are offline", {
+              toastId: "offline-id",
+            });
+          }
+        );
+
+      window &&
+        window.addEventListener("online", function (e) {
+          toast.success("You are back online", {
+            toastId: "online-id",
+          });
+        });
+    }
+    return () => {
+      if (typeof window !== "undefined") {
+        window &&
+          window.removeEventListener(
+            "your offline, please check your internet connection",
+            function (e) {
+              toast.error("You are offline", {
+                toastId: "offline-id",
+              });
+            }
+          );
+
+        window &&
+          window.removeEventListener("online", function (e) {
+            toast.success("You are back online", {
+              toastId: "online-id",
+            });
+          });
+      }
+    };
+  }, []);
+  return <div>{children}</div>;
+}
