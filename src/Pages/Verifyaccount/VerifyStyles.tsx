e5d44e5573440810abddd0083cffc6ca601c8a62
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-14 16:47:50 +0100

    Working on verify Email

---
+++ src/Pages/Verifyaccount/VerifyStyles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const VerifyWrap = styled.div`
+ height : 100vh;
+ background : red;
+ justify-content : center;
+ align-items : center;
+`
--- src/Pages/Verifyaccount/Verifyaccount.js
+++ src/Pages/Verifyaccount/Verifyaccount.js
@@ -2,14 +2,31 @@ import {React, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import VerificationInput from "react-verification-input";
+import { VerifyWrap } from "./VerifyStyles";
 
 
 const Verifyaccount = () => {
   const navigate = useNavigate();
   const {email } = useParams();
+  const [code, setCode] = useState("");
+
   console.log(email)
-  
-  return <h1>Hello from Verifyaccount</h1>;
+
+  return <VerifyWrap className="sec-flex">
+    <h4>Hello verify your email</h4>
+     <VerificationInput
+            length={7}
+            validChars={[/0-9/]}
+            onChange={setCode}
+            autoFocus={true}
+            classNames={{
+              container: "container",
+              character: "character",
+              characterInactive: "character--inactive",
+              characterSelected: "character--selected",
+            }}
+          />
+  </VerifyWrap>;
 };
 
 export default Verifyaccount;
